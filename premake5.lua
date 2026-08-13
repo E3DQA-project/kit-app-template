d531c1f735b34c3d12a6fc80cc9b44b31eabe47b
Pierce-Su <piercesu1028@gmail.com>
2026-08-13 18:44:30 +0800

    Clean active application build configuration

--- premake5.lua
+++ premake5.lua
@@ -16,8 +16,6 @@ repo_build.prebuild_copy {
 
 -- Apps: for each app generate batch files and a project based on kit files (e.g. my_name.my_app.kit)
 
-define_app("my_editor_1.kit")
-define_app("my_editor_clean.kit")
 define_app("nycu.e3dqa_scene_viewer.kit")
 define_app("nycu.mos_app.kit")
 
